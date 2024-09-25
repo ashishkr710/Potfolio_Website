2629c1062286d50831a2c40f409a779ee1d41fc8
Ashish kumar <ashishkr710@gmail.com>
2024-09-25 22:45:15 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,71 @@
+import {
+  algorithms,
+  devnotes,
+  oscs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "hero",
+    title: "Hero",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const experiences = [
+  {
+    title: "Software Engineer",
+    company_name: "SmartData",
+    date: "2024 - OnGoing",
+    details: [
+      "Built custom enterprise applications for a <span style='color: white;'>Multi-National clients </span> as a full-stack software engineer.",
+      "Developed and maintained <span style='color: white;'>scalable Full Stack services</span>, ensuring high availability for critical business applications.",
+      "<span style='color: white;'>Collaborated with a team</span> to design and implement front-end interfaces.",
+    ],
+  },
+  {
+    title: "Computer Science",
+    company_name:"MMDU",
+    date: "2020 - 2024",
+    details: [
+      // "Built a <span style='color: white;'>computer science foundation</span> learning theory, computer architecture, and software engineering.",
+      // "Worked and interned at <span style='color: white;'>NASA and Norfolk Southern Railway</span> to gain practical experience in the field of data analysis.",
+      // "Acted as a member of the <span style='color: white;'>Association for Computing Machinery</span> (ACM).",
+    ],
+  },
+];
+
+const portfolio = [
+  {
+    name: "Open Source Computer Science Repo",
+    description:
+      "A GitHub repo with over 17,000 stars containing a curated list of free online courses from reputable universities that satisfy undergraduate computer science requirements.",
+    image: oscs,
+  },
+  {
+    name: "Dev Notes",
+    description:
+      "A newsletter with over 6,000 readers made for software developers to keep up with this rapidly evolving industry, with a sister platform in progress.",
+    image: devnotes,
+  },
+  {
+    name: "Visually Understanding Algorithms",
+    description:
+      "A showcase of animated algorithms coded using TypeScript, with the video garnering over 400,000 views.",
+    image: algorithms,
+  },
+];
+
+export { experiences, portfolio };
+
